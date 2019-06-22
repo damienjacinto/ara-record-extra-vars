6f7e0f2a12554ee5be39cad6ebbceb1500ff60ed
Damien Jacinto <damien.jacinto@gmail.com>
2019-06-23 00:38:08 +0200

    cleanup code

--- zzz_extra_vars_ara.py
+++ zzz_extra_vars_ara.py
@@ -1,12 +1,23 @@
 from ansible.plugins.callback import CallbackBase
-import logging
-from pprint import pprint
 from ansible import context
+
+DOCUMENTATION = '''
+callback: extra_vars_to_ara_record
+type: notification
+short_description: add extra_vars to ara_record of the current playbook
+description:
+- Ansible callback to record extra_vars with ara_record
+- The name had to start with a z because the ara callback needs to start before this callback
+author:
+- Damien Jacinto
+requirements:
+- "python >= 2.6"
+- "ara >= 0.10.0 < 1.0.0"
+'''
 
 try:
   from ara import models
   from ara.models import db
-  from ara.webapp import create_app
   from flask import current_app
   HAS_ARA = True
 except ImportError:
@@ -15,16 +26,13 @@ except ImportError:
 class CallbackModule(CallbackBase):
   CALLBACK_VERSION = 2.0
   CALLBACK_TYPE = 'notification'
-  CALLBACK_NAME = 'sncf_jenkins'
+  CALLBACK_NAME = 'extra_vars_to_ara_record'
+  IGNORE_EXTRA_VARS = frozenset(('pass', 'mdp'))
 
   def __init__(self):
     super(CallbackModule, self).__init__()
-    logging.basicConfig(filename='example.log', filemode='w', format='%(asctime)s %(message)s', level=logging.DEBUG)
     if context.CLIARGS:
-      logging.debug('Cli ok')
       self.extra_vars = context.CLIARGS['extra_vars']
-    else:
-      logging.debug('cli non ok')
 
   def create_or_update_key(self, playbook_id, key, value, type):
     try:
@@ -48,9 +56,9 @@ class CallbackModule(CallbackBase):
       self.playbook_id = current_app._cache['playbook']
     
   def v2_playbook_on_play_start(self, play):
-    logging.debug('Play started')
     if self.playbook_id:
       self.extra_vars = play._variable_manager._extra_vars
       for extra_vars_key,extra_vars_value in self.extra_vars.items():
-        self.create_or_update_key(self.playbook_id, extra_vars_key, extra_vars_value, 'text')
+        if all(x not in extra_vars_key for x in self.IGNORE_EXTRA_VARS):
+          self.create_or_update_key(self.playbook_id, extra_vars_key, extra_vars_value, 'text')
         
