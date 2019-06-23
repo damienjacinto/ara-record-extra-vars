7722c0718191cff447aa4880448d917770c9db2f
Damien Jacinto <damien.jacinto@gmail.com>
2019-06-23 15:42:08 +0200

    Add type record

--- zzz_extra_vars_ara.py
+++ zzz_extra_vars_ara.py
@@ -8,11 +8,25 @@ short_description: add extra_vars to ara_record of the current playbook
 description:
 - Ansible callback to record extra_vars with ara_record
 - The name had to start with a z because the ara callback needs to start before this callback
+- You can force the type of the record with a suffix on the extra_vars key (see VALID_TYPES)
 author:
 - Damien Jacinto
 requirements:
 - "python >= 2.6"
 - "ara >= 0.10.0 < 1.0.0"
+'''
+
+EXAMPLES = '''
+# add the callback with the callback for ara
+# call your playbook with en extra_vars or an extra_vars file
+ansible-playbook -i inventory/ main.yml -e my_url='http://test.io'
+
+# In this exemple the extra_var key is suffixed by _url so the ara record's type will be url.
+# text is the default one, in the web ui _json and _dict get a pretty format, _list use the <ul><li> html.
+# All your extra_vars are added to the ara_record of the playbook.
+
+# You can ignore key that contains some values (see IGNORE_EXTRA_VARS).
+# If the ara callback is not called before this callback, this callback will not do anything.
 '''
 
 try:
@@ -28,9 +42,11 @@ class CallbackModule(CallbackBase):
   CALLBACK_TYPE = 'notification'
   CALLBACK_NAME = 'extra_vars_to_ara_record'
   IGNORE_EXTRA_VARS = frozenset(('pass', 'mdp'))
+  VALID_TYPES = ['text', 'url', 'json', 'list', 'dict']
 
   def __init__(self):
     super(CallbackModule, self).__init__()
+    self.extra_vars = []
     if context.CLIARGS:
       self.extra_vars = context.CLIARGS['extra_vars']
 
@@ -50,15 +66,22 @@ class CallbackModule(CallbackBase):
     db.session.add(data)
     db.session.commit()
     return data
+
+  def get_type_record(self, key):
+    for typeRecord in self.VALID_TYPES:
+      if key.endswith('_'+typeRecord):
+        return typeRecord
+    return self.VALID_TYPES[0]
   
   def v2_playbook_on_start(self, playbook):
     if HAS_ARA and current_app:
       self.playbook_id = current_app._cache['playbook']
     
   def v2_playbook_on_play_start(self, play):
-    if self.playbook_id:
+    if hasattr(self, 'playbook_id'):
       self.extra_vars = play._variable_manager._extra_vars
       for extra_vars_key,extra_vars_value in self.extra_vars.items():
         if all(x not in extra_vars_key for x in self.IGNORE_EXTRA_VARS):
-          self.create_or_update_key(self.playbook_id, extra_vars_key, extra_vars_value, 'text')
+          typeRecord = self.get_type_record(extra_vars_key)
+          self.create_or_update_key(self.playbook_id, extra_vars_key, extra_vars_value, typeRecord)
         
